602933e68a87c00dfe4a41a3d3b6a88acd0576a6
Kumar6174 <mrbadshah768@gmail.com>
2024-01-18 16:03:39 +0530

    Add project files.

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using EmployeManagement.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeManagement.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees{  get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
